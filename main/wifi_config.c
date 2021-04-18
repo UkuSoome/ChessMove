5651283dac92a0e7a5ecfac2e408d8d0a8c2edf1
UkuSoome <Soome.Uku@gmail.com>
2021-04-18 19:34:30 +0300

    httprequestfix

--- main/wifi_config.c
+++ main/wifi_config.c
@@ -180,7 +180,7 @@ void sendHttpRequest(char* urlString, char* move) {
     .event_handler = _http_event_handle,
     };
     esp_http_client_handle_t client = esp_http_client_init(&config);
-    //esp_http_client_set_method(client, HTTP_METHOD_POST);
+    esp_http_client_set_method(client, HTTP_METHOD_POST);
     //esp_http_client_set_header(client, "Content-Type", "application/json");
     esp_err_t err = esp_http_client_perform(client);
 
