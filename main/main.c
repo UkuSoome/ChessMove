45ea9061fe5b58c2c9a5bd3ac82ec9678c16c19c
UkuSoome <Soome.Uku@gmail.com>
2021-05-03 02:57:20 +0300

    httprequestfix

--- main/main.c
+++ main/main.c
@@ -21,7 +21,11 @@
 
 int checkboard[8][8] = {};
 
-
+int fromnumb;
+int fromlet;
+int tonumb;
+int tolet;
+bool movedone;
 void startGame() {
     char* starturl;
     asprintf(&starturl, "http://%s:24377/api/start/%d", SERVERIP, BOARDID);
@@ -50,6 +54,33 @@ void printboard(void) {
         printf("\n");
     }
 }
+void compareBoards(void) {
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {   
+            if (checkboard[i][j] == 1 && button_matrix[i][j] = 0) {
+                fromnumb = j+1;
+                fromlet = i+1;
+                movedone = true;
+            }
+            else if(checkboard[i][j] == 0 && button_matrix[i][j] == 1) {
+                to = j+1;
+                tolet = i+1;
+                movedone = true;
+            }
+        }
+    }
+}
+
+char letterFromRow(int row) {
+    if (row==1) return 'a';
+    if (row==2) return 'b';
+    if (row==3) return 'c';
+    if (row==4) return 'd';
+    if (row==5) return 'e';
+    if (row==6) return 'f';
+    if (row==7) return 'g';
+    if (row==8) return 'h';
+}
 void app_main(void)
 {   
     for (int i = 0; i < 8; ++i) {
@@ -62,28 +93,25 @@ void app_main(void)
             }
         }
     }
-    /*configure_wifi();
+    configure_wifi();
     vTaskDelay(1000/ portTICK_PERIOD_MS);
-    char pos1 = 'a';
-    char pos2 = '2';
-    char pos3 = 'a';
-    char pos4 = '4';
+    //char pos1 = 'a';
+    //char pos2 = '2';
+    //char pos3 = 'a';
+    //char pos4 = '4';
     startGame();
     vTaskDelay(1000/ portTICK_PERIOD_MS);
-    char* move = buildMove(pos1, pos2, pos3, pos4);
-    sendMove(move);
+    char* move;// = buildMove(pos1, pos2, pos3, pos4);
+    //sendMove(move);
 
-    while (1) {
-        printf("tere\n");
-        vTaskDelay(1000/ portTICK_PERIOD_MS);    // Wait at least 100ms
-    }*/
     static const char *SPI_TAG = "MAIN";
     uint8_t numb_of_devices = 8;
     device device_arr[numb_of_devices];
     configure_spi(numb_of_devices, device_arr);
     uint16_t print_counter = 0;
     int64_t prev_time = 0;
-    int** buttons;
+    char pos1;
+    char pos2;
     while (1) {
         if (QT_MU_1_2_INT_FLAG || QT_MU_3_4_INT_FLAG || QT_SU_1_2_INT_FLAG || QT_SU_3_4_INT_FLAG || QT_INT_ERR_FLAG) {
             if (QT_MU_1_2_INT_FLAG == true) {
@@ -111,13 +139,21 @@ void app_main(void)
             }
         }
         vTaskDelay(100/ portTICK_PERIOD_MS);    // Wait at least 100ms
-        if (esp_timer_get_time()-prev_time >= PRINT_BOARD_INTERVAL_US) {
+        compareBoards();
+        if (movedone) {
+            pos1 = letterFromRow(fromlet);
+            pos2 = letterFromRow(tolet);
+            move = buildMove(pos1, fromnumb, pos2, tonumb);
+            sendMove(move);
+            movedone = false;
+        }
+        /*if (esp_timer_get_time()-prev_time >= PRINT_BOARD_INTERVAL_US) {
             prev_time = esp_timer_get_time();
             //printf("Print number: %d\n", print_counter++);
             //print_board();
             //buttons = getButtonMatrix();
             printboard();
-        }
+        }*/
     }
 }
 
