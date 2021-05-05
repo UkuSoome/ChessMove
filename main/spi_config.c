5bd9f159590ef32b233fe7b4775e342dd4b90f2b
UkuSoome <Soome.Uku@gmail.com>
2021-05-05 22:32:16 +0300

    httprequestfix

--- main/spi_config.c
+++ main/spi_config.c
@@ -350,8 +350,8 @@ void QT_setup(device qt_device){
     QT_setup_register(qt_device, REG_KEY8_NTHR, CMD_DISABLE_KEY);
     QT_setup_register(qt_device, REG_KEY9_NTHR, CMD_DISABLE_KEY);
     QT_setup_register(qt_device, REG_KEY10_NTHR, CMD_DISABLE_KEY);
-    QT_setup_register(qt_device, 0x96, 0x16);
-    //QT_control_command(qt_device, 0x03);
+    QT_setup_register(qt_device, 0x96, 0x32);
+    QT_control_command(qt_device, 0x03);
     //QT_control_command(qt_device, 0x0A);
 }
 void QT_report_request(device qt_device, uint8_t command, uint8_t rec_length)
