b53bb1e9c20172a1da0af30b5c026aba7ebd0d35
UkuSoome <Soome.Uku@gmail.com>
2021-05-07 00:05:24 +0300

    httprequestfix

--- main/main.c
+++ main/main.c
@@ -88,8 +88,6 @@ void app_main(void)
             }
         }
     }
-    printboard();
-    print_board();
     configure_wifi();
     vTaskDelay(1000/ portTICK_PERIOD_MS);
 
@@ -103,36 +101,15 @@ void app_main(void)
     int64_t prev_time = 0;
     char pos1;
     char pos2;
-    //vTaskDelay(40000/ portTICK_PERIOD_MS);
     int nupud_korras = 0;
     int count_nupud = 0;
-   /* while (nupud_korras != 32) {
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (button_matrix[i][j] == 1) {
-                    count_nupud++;
-                }
-            }
-        }
-        ESP_LOGI(SPI_TAG, "NUPPE LOETUD - %x", count_nupud);
-        nupud_korras = count_nupud;
-        ESP_LOGI(SPI_TAG, "NUPPE KORRAS - %x", nupud_korras);
-        print_board();
-        printf("\n");
-        printf("\n");
-        for (int i = 0; i < numb_of_devices; i++) {
-            QT_check_buttons_and_update_board(device_arr[i]);
-        }
-        count_nupud = 0;
-        vTaskDelay(1000/ portTICK_PERIOD_MS);
-    }*/
-    print_board();
+
     printf("START");
     printf("\n");
     startGame();
     vTaskDelay(1000/ portTICK_PERIOD_MS);
     while (1) {
-        if (QT_MU_1_2_INT_FLAG || QT_MU_3_4_INT_FLAG || QT_SU_1_2_INT_FLAG || QT_SU_3_4_INT_FLAG || QT_INT_ERR_FLAG) {
+        /*if (QT_MU_1_2_INT_FLAG || QT_MU_3_4_INT_FLAG || QT_SU_1_2_INT_FLAG || QT_SU_3_4_INT_FLAG || QT_INT_ERR_FLAG) {
             if (QT_MU_1_2_INT_FLAG == true) {
                 QT_MU_1_2_INT_FLAG = false;
                 QT_check_buttons_and_update_board(device_arr[0]);
@@ -162,7 +139,8 @@ void app_main(void)
                 checkFrom = 0;
                 checkTo = 0;
             }
-        }
+        }*/
+        check_buttons(device_arr);
         
             /*for (int i = 0; i < 8; ++i) {
                  QT_check_buttons_and_update_board(device_arr[i]);
@@ -193,7 +171,7 @@ void app_main(void)
             //printf("Print number: %d\n", print_counter++);
             //print_board();
             //buttons = getButtonMatrix();
-            //print_board();
+            print_board();
             printf("\n");
             printf("\n");
             printf("\n");
--- main/spi_config.c
+++ main/spi_config.c
@@ -470,22 +470,22 @@ void QT_check_buttons_and_update_board(device qt_device) {
     //ESP_LOGI(SPI_TAG, "Button row data: %x for device %s", button_row_data, qt_device.name);
     for (int i = 0; i < BUTTON_MATRIX_COL_SIZE; ++i) {
         if ((button_row_data & (0x01<<i))>>i) {
-            //if (button_matrix[qt_device.row_index][i] == 0) {
+            /*if (button_matrix[qt_device.row_index][i] == 0) {
                 toLet = letterFromRow(i);
                 toNumb = qt_device.row_index+1;
                 ESP_LOGI(SPI_TAG, "SIIA TEHTI KÄIK: %C%X", toLet,toNumb);
                 checkTo++;
-            //}
+            }*/
             button_matrix[qt_device.row_index][i] = 1;
             //ESP_LOGI(SPI_TAG, "siin real %x on nupp %x staatuses UKS", qt_device.row_index+1, i+1);
         }
         else {
-            if (button_matrix[qt_device.row_index][i] == 1) {
+            /*if (button_matrix[qt_device.row_index][i] == 1) {
                 fromLet = letterFromRow(i);
                 fromNumb = qt_device.row_index+1;
                 ESP_LOGI(SPI_TAG, "SIIT TEHTI KÄIK: %C%X", fromLet,fromNumb);
                 checkTo++;
-            }
+            }*/
             button_matrix[qt_device.row_index][i] = 0;
             //ESP_LOGI(SPI_TAG,"siin real %x on nupp %x staatuses NULL", qt_device.row_index+1, i+1);
         }    
