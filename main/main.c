60fba864508134a046337870fac7e783d47fe0d0
UkuSoome <Soome.Uku@gmail.com>
2021-06-06 20:36:25 +0300

    httprequestfix

--- main/main.c
+++ main/main.c
@@ -106,10 +106,10 @@ void app_main(void)
             sendMove(move);
             ESP_LOGI("DEBUG","MOVE DONE - %s", move);
             checkTo = 0;
-            fromLet = 'x';
-            toLet = 'x';
-            fromNumb = 0;
-            toNumb = 0;
+            //fromLet = 'x';
+            //toLet = 'x';
+            //fromNumb = 0;
+            //toNumb = 0;
         }
         if (esp_timer_get_time()-prev_time >= PRINT_BOARD_INTERVAL_US) {
             prev_time = esp_timer_get_time();
